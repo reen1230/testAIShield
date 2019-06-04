bf44495a7bc1120516fd40ecd413c2209e8ceba2
reen cho <reen.cho@zyxel.com.tw>
2019-06-04 17:02:57 +0800

    init framework

---
+++ testPod/testPod/testPod.h
@@ -0,0 +1,19 @@
+//
+//  testPod.h
+//  testPod
+//
+//  Created by reen cho on 2019/6/4.
+//  Copyright © 2019 reen cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for testPod.
+FOUNDATION_EXPORT double testPodVersionNumber;
+
+//! Project version string for testPod.
+FOUNDATION_EXPORT const unsigned char testPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testPod/PublicHeader.h>
+
+
